e503cb307097d6b90be1b6e8732944abffd50371
Hermi19 <hteshome1619@gmail.com>
2023-02-19 06:05:20 -0800

    Task 8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main- Entry point
+ *
+ * Description: Print numbers of base 16 in lowercase
+ * Return: Zero value
+ */
+
+int main(void)
+{
+	char c;
+	int n;
+
+	for (n = '0'; n <= '9'; n++)
+		putchar(n);
+	for (c = 'a'; c <= 'f'; c++)
+		putchar(c);
+	putchar('\n');
+	return (0);
+}
